cbaf74e431bba0b48dadd716329549fa11e649ef
OusmaneKana <kanaman6@gmail.com>
2020-06-17 00:57:53 -0500

    poll app and views update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("Hello Mofockas")
 
+def detail(request, question_id):
+	return HttpResponse("You're looking at question %s." %question_id)
+
+def results(request, question_id):
+	response ="You're looking at question %s" %question_id
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." %question_id)
+
 # Create your views here.
